8b2c5bb7d6605805d7dddc9e34dc3bc9175c4017
Shita Zeny <shita0639@gmail.com>
2025-02-22 19:01:00 +0700

    refactor: changes code view data #4

--- shita-next/src/app/person/read/[id]/page.tsx
+++ shita-next/src/app/person/read/[id]/page.tsx
@@ -2,30 +2,62 @@
 import { fetcher } from '@/app/libs'
 import { use } from 'react';
 import useSWR from 'swr';
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+} from "@/components/ui/card"
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 
 export default function Detail({ params }: { params: Promise<{ id: number }> }) {
-
     const resolvedParams = use(params);
     const { data: person, isLoading, error } = useSWR(`/utils/queries/person/${resolvedParams.id}`, fetcher);
+    const router = useRouter();
 
     if (isLoading) return <div><span>Loading...</span></div>;
     if (error) return <div><span>Error fetching data</span></div>;
     if (!person) return <div><span>No user found</span></div>;
 
-    if(person) {
+    if (person) {
         console.info("Person Data: ", person)
     }
-    
+
     return (
-        <div className='w-full'>
-            <h2 className='text-center font-bold text-3xl py-3'>{person.name}</h2>
-            <div className='w-full max-w-4xl m-auto border-[1px] p-3 border-gray-500 rounded-md'>
-                <p >{person.address}</p>
-            </div>
-            <div className='w-full max-w-4xl m-auto border-[1px] p-3 border-gray-500 rounded-md'>
-                <p >{person.phone}</p>
-            </div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <span className='font-bold py-2 block text-2xl text-center mb-5'>View Person Data</span>
+            <Card className="w-[450px]">
+                <CardHeader></CardHeader>
+
+                <CardContent>
+                    <form>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">Name</Label>
+                                <Input id="name" value={person.name} readOnly />
+                            </div>
+
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="address">Address</Label>
+                                <Input id="address" value={person.address} readOnly />
+                            </div>
+
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="phone">Phone</Label>
+                                <Input id="phone" value={person.phone} readOnly />
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+
+                <CardFooter className="flex justify-between">
+                    <Button onClick={() => router.push("/person")}>Close</Button>
+                </CardFooter>
+            </Card>
         </div>
+        
     );
 }
-
